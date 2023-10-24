db05ae5bc63e608b64edd65107ed657e8c58bc48
Santiago Ivan Alvarez <santialva112@gmail.com>
2023-10-24 11:40:46 -0300

    primer commit

--- frontend/naturepic/src/App.jsx
+++ frontend/naturepic/src/App.jsx
@@ -1,24 +1,34 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Header from './Components/Header/Header'
-import Admin from './Components/Admin/Admin'
-import Home from './Components/Home/Home'
-import Detalle from './Components/Detalle/Detalle'
-import Footer from './Components/Footer/Footer'
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Detalle from './Components/Detalle/Detalle';
+import Footer from './Components/Footer/Footer';
+import Admin from './Components/Admin/Admin';
+import AgregarProducto from './Components/Admin/AgregarProducto';
+import ListarProductos from './Components/Admin/ListarProductos';
+
 
 function App() {
-
   return (
     <>
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/admin' element={<Admin/>}/>
-      <Route path='/detalle/:productId' element={<Detalle/>}/>
-    </Routes>
-    <Footer/>
+      <Header />
+      <div className='main-content'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/detalle/:productId' element={<Detalle />} />
+        {/* Rutas de Administración */}
+        <Route path='/admin' element={<Admin />}>
+          <Route path='agregar-producto' element={<AgregarProducto />} />
+          <Route path='lista-de-productos' element={<ListarProductos />} />
+        </Route>
+      </Routes>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
--- frontend/naturepic/src/Components/Admin/Admin.jsx
+++ frontend/naturepic/src/Components/Admin/Admin.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-const Admin = () => {
-
+function Admin() {
   return (
     <div>
+      <h1>Panel de Administración</h1>
+      <nav>
+        <Link to='agregar-producto'>Agregar Producto</Link>
+        <Link to='lista-de-productos'>Listar Productos</Link>
+      </nav>
+      <Outlet /> {/* Esto renderizará el componente correspondiente a la ruta anidada */}
     </div>
   );
-};
+}
 
 export default Admin;
